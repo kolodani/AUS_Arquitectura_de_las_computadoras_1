ba88e06151dcf58709884b731511fcd8ecbe75d3
kolodani <danielvinzia@gmail.com>
2023-05-02 16:50:03 -0300

    assembly, 020 getting user input doubles

---
+++ 999-mips/020-getting_user_input_doubles.asm
@@ -0,0 +1,20 @@
+.data
+    promt: .asciiz "Enter the value of e: "
+    zeroDouble: .double 0.0
+.text
+    # Set the zero
+    lwc1 $f4, zeroDouble
+
+    # Display message to the user
+    li $v0, 4
+    la $a0, promt
+    syscall
+
+    # Get the double from the user
+    li $v0, 7
+    syscall
+
+    # Display the user's input
+    li $v0, 3
+    add.d $f12, $f0, $f4
+    syscall
