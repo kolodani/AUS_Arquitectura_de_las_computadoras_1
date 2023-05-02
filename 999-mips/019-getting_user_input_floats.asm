062249431a623706fd40c22b2c4fce6884982ca2
kolodani <danielvinzia@gmail.com>
2023-05-02 16:24:36 -0300

    assembly, 019 getting user input floats

---
+++ 999-mips/019-getting_user_input_floats.asm
@@ -0,0 +1,19 @@
+.data
+    message:    .asciiz "Enter the value of PI: "
+    zeroFloat:  .float 0.0
+.text
+    lwc1 $f4, zeroFloat    # cargo el cero en el registro $f4
+
+    # print message
+    li $v0, 4              # imprimo el mensaje
+    la $a0, message
+    syscall
+
+    # read user input
+    li $v0, 6              # leo el valor de PI
+    syscall
+
+    # display value
+    li $v0, 2              # imprimo el valor de PI
+    add.s $f12, $f0, $f4   # sumo el cero con el valor de PI
+    syscall
