3316eca9cc5b8ebf6e69910982ed9281c5ef2c01
kolodani <danielvinzia@gmail.com>
2023-05-16 14:37:45 -0300

    assembly, TP01, cuestion 1.3

---
+++ 10-TP01/002-Declaracion_de_palabras_en_memoria.asm
@@ -0,0 +1,7 @@
+.data         0x10010000
+palabras:    .word 15, 0x15      # decimal/hexadecimal
+
+# cuestion 1.3
+# los valores de las etiquetas son las direcciones de memoria y son iguales
+# en los dos programas
+
