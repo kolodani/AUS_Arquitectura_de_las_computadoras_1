16a588f37f4ec52c90c074e7d74b0bab59a98f51
kolodani <danielvinzia@gmail.com>
2023-05-16 14:25:34 -0300

    assembly, TP01, cuestion 1.2

--- 10-TP01/001-Declaracion_de_palabras_en_memoria.asm
+++ 10-TP01/001-Declaracion_de_palabras_en_memoria.asm
@@ -6,3 +6,9 @@ palabra2:    .word 0x15      # hexadecimal
 # el valor hexadecimal de palabra1 es 0x0000000F
 # el valor hexadecimal de palabra2 es 0x00000015
 
+# cuestion 1.2:
+# el valor de la palabra1 se encuentra almacenado en la direccion 0x10010000
+# el valor de la palabra2 se encuentra almacenado en la direccion 0x10010004
+# se encuentran en ese orden porque por defecto lo que se guarda en memoria
+# empieza en el valor 0x10010000 y va aumentando, en este caso de 4 en 4 porque
+# son palabras de 32 bits
