2589599b3d75f0e11dfc2ffe7e1c24b837cc9975
kolodani <danielvinzia@gmail.com>
2023-05-16 15:28:47 -0300

    assembly, TP01, cuestion 1.9

---
+++ 10-TP01/005-declaracion_de_bytes_en_memoria.asm
@@ -0,0 +1,9 @@
+            .data
+palabra1:   .byte       0x10, 0x20, 0x30, 0x40    # hexadecimal
+palabra2:   .word       0x10203040                # hexadecimal
+
+# cuestion 1.9
+# los valores almacenados en con la etiqueta palabra1 se ve en
+# memoria como: 0x40302010
+# mientras que el valor almacenado en con la etiqueta palabra2 se ve en 
+# memoria como: 0x10203040
