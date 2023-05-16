2bb96f747fdf1234cf5c0be5fe39c339c5eb3ae1
kolodani <danielvinzia@gmail.com>
2023-05-16 16:58:36 -0300

    assembly, TP01, cuestion 1.18

--- 10-TP01/009-Alineacion_de_datos_en_memoria.asm
+++ 10-TP01/009-Alineacion_de_datos_en_memoria.asm
@@ -9,3 +9,9 @@ palabra:    .word    10
 # despues de la variable byte1 que ocupa 1 byte
 # la variable espacio empieza en la direccion 0x10000001
 # y termina en la direccion 0x10000005
+
+# cuestion 1.18
+# los 4 bytes de espacio pueden constituir el espacio para una palabra
+# pero en este caso no, porque al tener un byte ocupado por byte1,
+# la variable word no puede empezar en la direccion 0x10000001,
+# si o si se corre y comienza en la direccion 0x10000004
