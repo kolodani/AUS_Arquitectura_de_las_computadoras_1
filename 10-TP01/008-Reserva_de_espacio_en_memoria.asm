3d1a66761d5c5b6acbbd73ea9e7b56d409d2ba09
kolodani <danielvinzia@gmail.com>
2023-05-16 16:24:46 -0300

    assembly, TP01, cuestion 1.15

---
+++ 10-TP01/008-Reserva_de_espacio_en_memoria.asm
@@ -0,0 +1,8 @@
+                .data
+palabra1:       .word        0x20
+espacio:        .space       8        # reservo espacio
+palabra2:       .word        0x30
+
+# cuestion 1.15
+# la variable espacio empieza en la direccion 0x10000004
+# y termina en la direccion 0x1000000B ocupando 8 bytes
