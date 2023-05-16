98a86c7cb24947446cb39d9da6206b4b1bf0a74b
kolodani <danielvinzia@gmail.com>
2023-05-16 16:03:40 -0300

    assembly, TP01, cuestion 1.13

--- 10-TP01/006-Declaracion_de_cadena_de_caracteres.asm
+++ 10-TP01/006-Declaracion_de_cadena_de_caracteres.asm
@@ -1,8 +1,16 @@
             .data
-cadena:     .ascii "abcde"        # defino string
+cadena:     .asciiz "abcde"        # defino string
 octeto:     .byte   0xff
 
 # cuestion 1.12
 # la cadena de caracteres se encuentra en la direccion de memoria 0x10010000
 # y continua hasta 0x10010005 esto incluye las 5 letras
 # y luego en la direccion 0x10010006 se encuentra el 0xff
+
+# cuestion 1.13
+# si se emplea asciiz en vez de ascii, el compilador agrega un 0 al final de
+# la cadena o tambien llamado fin de cadena que es el caracter \0,
+# por lo tanto la cadena de caracteres se encuentra en la direccion de
+# memoria 0x10010000 y continua hasta 0x10010006 esto incluye las 5 letras
+# y el fin de cadena
+# luego en la direccion 0x10010007 se encuentra el 0xff
