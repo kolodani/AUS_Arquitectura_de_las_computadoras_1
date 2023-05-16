f81dcd82a686a367476ae41fdda5cd3d06be1204
kolodani <danielvinzia@gmail.com>
2023-05-16 15:18:19 -0300

    assembly, TP01, cuestion 1.8

--- 10-TP01/004-declaracion_de_bytes_en_memoria.asm
+++ 10-TP01/004-declaracion_de_bytes_en_memoria.asm
@@ -3,3 +3,7 @@ octeto:     .byte    0x10    # hexadecimal
 
 # cuestion 1.7
 # la dirrecion de memoria donde se inicializo la variable octeto es 0x10010000
+
+# cuestion 1.8
+# el valor que se almacena en la memoria es 0x10 en hexadecimal
+# que es 16 en decimal
