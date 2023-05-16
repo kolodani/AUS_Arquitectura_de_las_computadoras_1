0bd88ba5e1682712e3f1fbc4fcac7cbc167d58a1
kolodani <danielvinzia@gmail.com>
2023-05-16 14:44:07 -0300

    assembly, TP01, cuestion 1.5

---
+++ 10-TP01/003-Declaracion_de_palabras_en_memoria.asm
@@ -0,0 +1,6 @@
+.data         0x10000000
+vector:       .word 0x10, 30, 0x34, 0x20, 60
+
+# cuestion 1.5
+# los numeros se guardan de forma correcta pero al comenzar el .data
+# en 0x10000000, se estan guardando en .extern
