1f8646878e007a4b33b5d0b2165fa5fb824d3ecc
kolodani <danielvinzia@gmail.com>
2023-05-16 16:08:02 -0300

    assembly, TP01, cuestion 1.14

---
+++ 10-TP01/007-Declaracion_de_cadena_de_caracteres.asm
@@ -0,0 +1,7 @@
+            .data
+cadena:     .byte   "abcde"        # defino string
+octeto:     .byte   0xff
+
+# cuestion 1.14
+# al usar la directiva .byte para la cadena de caracteres,
+# el simulador da error, porque no puede interpretar el string como un numero
