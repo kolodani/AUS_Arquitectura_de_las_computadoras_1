cd53bdcd88a0a5a6e3c18cfc22e261d9a9da1cf0
kolodani <danielvinzia@gmail.com>
2023-05-16 17:22:16 -0300

    assembly, TP01, cuestion 1.21

---
+++ 10-TP01/010-Alineacion_de_datos_en_memoria.asm
@@ -0,0 +1,10 @@
+            .data
+byte1:      .byte    0x10
+            .align   2
+espacio:    .space   4
+byte2:      .byte    0x20
+palabra:    .word    10
+
+# cuestion 1.21
+# la variable espacio se encuentra en la direccion 0x10010004
+# y termina en la direccion 0x10010007
