e4bbb311a776bd3c85a45f203b15851c43779439
kolodani <danielvinzia@gmail.com>
2023-05-16 14:38:27 -0300

    assembly, TP01, cuestion 1.4

--- 10-TP01/002-Declaracion_de_palabras_en_memoria.asm
+++ 10-TP01/002-Declaracion_de_palabras_en_memoria.asm
@@ -5,3 +5,7 @@ palabras:    .word 15, 0x15      # decimal/hexadecimal
 # los valores de las etiquetas son las direcciones de memoria y son iguales
 # en los dos programas
 
+# cuestion 1.4
+# la diferencia entre los dos programas es las estiquetas que se usan son mas
+# facil de acceder ya que tienen nombres individuales y eso no sucede en este
+# programa
