16b3b40a794012f8c05cf99e1eebda5e126a88b0
kolodani <danielvinzia@gmail.com>
2023-05-16 17:27:30 -0300

    assembly, TP01, cuestion 1.22

--- 10-TP01/010-Alineacion_de_datos_en_memoria.asm
+++ 10-TP01/010-Alineacion_de_datos_en_memoria.asm
@@ -8,3 +8,10 @@ palabra:    .word    10
 # cuestion 1.21
 # la variable espacio se encuentra en la direccion 0x10010004
 # y termina en la direccion 0x10010007
+
+# cuestion 1.22
+# en cuatro bytes se puede construir una palabra ya que
+# la misma ocupa esa cantidad de bytes.
+# .aling lo que asi es alinear la direccion de memoria
+# lo que significa que si se pone .align n,
+# n es el numero de bytes que se alinearan
