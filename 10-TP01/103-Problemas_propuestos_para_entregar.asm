90ff2320250848b5af180c634d9cb31f05d78d17
kolodani <danielvinzia@gmail.com>
2023-05-17 14:18:01 -0300

    assembly, TP01, entrega 3

---
+++ 10-TP01/103-Problemas_propuestos_para_entregar.asm
@@ -0,0 +1,9 @@
+.data           0x10001000
+# alinearlas en direcciones multiplo de 4
+
+palabra:        .align      4
+                .word       1
+byte:           .align      4
+                .byte       2
+palabra2:       .align      4
+                .word       3
