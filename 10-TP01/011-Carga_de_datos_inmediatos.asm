bc35fe79e41f4252aaba297f3acc0a3cc9526d0f
kolodani <danielvinzia@gmail.com>
2023-05-17 15:27:06 -0300

    assembly, TP01, cuestion 2.2

--- 10-TP01/011-Carga_de_datos_inmediatos.asm
+++ 10-TP01/011-Carga_de_datos_inmediatos.asm
@@ -7,3 +7,7 @@ main:           lui $s0, 0x8690
 # el tamaño es de 32 bits, por lo que completa con ceros los 16 bits restantes
 # en la direccion 0x00400000 se encuentra esto: 0x3c108690
 # que es el valor 8690 que ingrese y el 3c10 es el opcode de lui
+
+# cuestion 2.2
+
+# en la zona de los registro se encuentra el valor 0x86900000 en el registro $s0
