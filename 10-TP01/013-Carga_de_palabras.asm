a50f6df21ae8ed2dcfdb58c1d210d09dda0dfc24
kolodani <danielvinzia@gmail.com>
2023-05-17 15:53:56 -0300

    assembly, TP01, cuestion 2.6

--- 10-TP01/013-Carga_de_palabras.asm
+++ 10-TP01/013-Carga_de_palabras.asm
@@ -9,3 +9,16 @@ main:       lw $s0, palabra($0)
 # palabra 0x10203040, en el espacio de memoria 0x10010000.
 # luego se ejecuta la instruccion lw y
 # se carga en el registro $s0 el valor 0x10203040
+
+# cuestion 2.6
+
+# lo que hace el simulador es cargar en el registro $s0 el valor que este en la
+# memoria, como hace esto?. primero se fija en el registro $0 que tiene el
+# valor 0, luego se fija en la palabra que esta en la memoria de datos, en la
+# direccion 0x10010000, y como el registro $0 tiene el valor 0, entonces se
+# carga en el registro $s0 el valor que esta en la direccion 0x10010000, que
+# es 0x10203040. ademas usa tres instrucciones para poder hacer el proceso,
+# que son las siguientes: lui, addu y lw. con lui carga en el registro $at el
+# valor 0x1001, luego con addu carga en el registro $at el valor 0x10010000,
+# y por ultimo con lw carga en el registro $s0 el valor que esta en la
+# direccion 0x10010000, que es 0x10203040.
