329084de0f57280285378984271eaa40217ab2e3
kolodani <danielvinzia@gmail.com>
2023-05-17 15:36:10 -0300

    assembly, TP01, cuestion 2.4

--- 10-TP01/012-Carga_de_datos_inmediatos.asm
+++ 10-TP01/012-Carga_de_datos_inmediatos.asm
@@ -6,3 +6,8 @@ main:               li $s0, 0x12345678
 # cuando se ejecuta el programa se inicializan los registros
 # $s0 = 0x12345678 y $at = 0x12340000
 # en el primer paso se inicializa $at y luego $s0
+
+# cuestion 2.4
+
+# cuando el programa es ejecutado se realizan dos instrucciones
+# las instrucciones son: lui y ori
