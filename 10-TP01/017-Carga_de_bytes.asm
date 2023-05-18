05597af8899cf1489f5d3d103d310f1f931e0904
kolodani <danielvinzia@gmail.com>
2023-05-18 11:35:24 -0300

    assembly, TP01, cuestion 2.13

--- 10-TP01/017-Carga_de_bytes.asm
+++ 10-TP01/017-Carga_de_bytes.asm
@@ -12,3 +12,10 @@ main:           lb $s0, octeto($0)
 # sigue funcionando sin ningun problema.
 # lo que sucede tambien es que el simulador trabaja con
 # complemento a 2 y el valor de octeto es 0xf3 pasa a ser -13
+
+# cuestion 2.13
+
+# el programa no se ejecuta correctamente ya que el valor de octeto no es
+# correcto por sufrir un desbordamiento, pero el programa lo que hace es tomar
+# el numero que queda y seguir con el proceso llevando al espacio de los
+# registros un valor erroneo.
