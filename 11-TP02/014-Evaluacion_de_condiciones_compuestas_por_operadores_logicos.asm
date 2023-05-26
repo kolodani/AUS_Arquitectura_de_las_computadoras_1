9e1809bb1dddd5f7ac4028da0470f4ac9a92fd4b
kolodani <danielvinzia@gmail.com>
2023-05-26 18:07:26 -0300

    assembly, TP02, cuestion 1.16

---
+++ 11-TP02/014-Evaluacion_de_condiciones_compuestas_por_operadores_logicos.asm
@@ -0,0 +1,25 @@
+            .data
+dato1:      .word       0
+dato2:      .word       0
+res:        .space      1
+            .text
+main:
+        lw   $t8, dato1($0)
+        lw   $t9, dato2($0)
+        and  $t0, $t0, $0
+        and  $t1, $t1, $0
+        beq  $t8, $t0, igual
+        ori  $t0, $t0, 1
+igual:
+        beq  $t9, $0, fineval
+        ori  $t1, $0, 1
+fineval:
+        and  $t0, $t0, $t1
+        sb   $t0, res($0)
+
+# cuestion 1.16
+
+# el resultado que se guarda en res es 0 porqu, el primer beq se cumple y no se
+# le cambia el valor a $t0. despues en igual se cumple el segundo beq y no se le
+# cambia el valor a $t1. por ultimo se hace un and entre $t0 y $t1 que son 0 y
+# se guarda en res.
