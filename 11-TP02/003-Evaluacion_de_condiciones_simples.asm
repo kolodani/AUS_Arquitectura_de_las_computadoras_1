9ea453c547d9e559a40a81934ea8cbcfc0e54e39
kolodani <danielvinzia@gmail.com>
2023-05-26 16:14:29 -0300

    assembly, TP02, cuestion 1.4

---
+++ 11-TP02/003-Evaluacion_de_condiciones_simples.asm
@@ -0,0 +1,15 @@
+            .data
+dato1:      .word       60
+dato2:      .word       60
+res:        .space      1
+
+            .text
+main:
+        lw   $t0, dato1($0)    # cargar dato1 en t0
+        lw   $t1, dato2($0)    # cargar dato2 en t1
+        seq  $t2, $t0, $t1     # pone 1 $t2 si t0 = t1
+        sb   $t2, res($0)      # almacenar $2 en res
+
+# cuestion 1.4
+
+# el codigo se modifico de la instruccion slt de antes por la instruccion seq
