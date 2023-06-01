08b67f5f10904ba64ea2955faf3751994f466a6d
kolodani <danielvinzia@gmail.com>
2023-06-01 15:32:46 -0300

    assembly, TP03, cuestion 1.5

---
+++ 12-TP03/005-Impresion_de_enteros.asm
@@ -0,0 +1,14 @@
+            .data
+dir:        .asciiz   "Se va a imprimir el entero: "
+entero:     .word     7
+
+            .text
+main:
+        li    $v0, 4       # codigo de imprimir cadena
+        la    $a0, dir     # direccion de la cadena
+        syscall            # llamada al sistema
+        li    $v0, 1       # codigo de imprimir entero
+        lw    $a0, entero  # entero a imprimir
+        syscall            # llamada al sistema
+
+# cuestion 1.5
