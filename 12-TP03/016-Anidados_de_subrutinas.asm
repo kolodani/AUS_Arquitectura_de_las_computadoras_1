ad6bb9251d24352d903d4ad887cc4f65e7df35ae
kolodani <danielvinzia@gmail.com>
2023-06-06 15:18:22 -0300

    assembly, TP03, cuestion 2.9

--- 12-TP03/016-Anidados_de_subrutinas.asm
+++ 12-TP03/016-Anidados_de_subrutinas.asm
@@ -25,3 +25,8 @@ subr:
 # despues de la ejecucion de la instruccion jal main, el contenido de $ra es
 # 0x00400024 y el de pc cambia de 0x00400000 a 0x00400040 saltando de 4 en 4 y
 # vuelve a 0x00400024 y enpieza a saltar de vuelta en 4 en 4 hasta 0x00400038
+
+# cuestion 2.9
+
+# en el tope de la pila se almacena el valor de $ra que en el momento que
+# comienza la ejecucion del programa es 0x00000000
