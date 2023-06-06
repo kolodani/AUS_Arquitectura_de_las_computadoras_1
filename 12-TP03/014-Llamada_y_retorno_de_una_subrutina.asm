2fa9f4292c23542b51da34d9126483bb13771f0b
kolodani <danielvinzia@gmail.com>
2023-06-06 11:14:12 -0300

    assembly, TP03, cuestion 2.6

--- 12-TP03/014-Llamada_y_retorno_de_una_subrutina.asm
+++ 12-TP03/014-Llamada_y_retorno_de_una_subrutina.asm
@@ -7,5 +7,14 @@ main:
 
 # cuestion 2.5
 
-# el contenido de PC y del registro $ra antes y despues de la instruccion jal
-# main es cero
+# el contenido del registro $ra antes y despues de la instruccion jal main es
+# cero. el contenido de pc es 0x00400000 cuando comienza la ejecucion, y despues
+# de jal main es 0x00400004 cuando se ejecuta la instruccion li $a0, 10, luego
+# es 0x00400008 cuando se ejecuta la instruccion li $a1, 20, luego es 0x0040000c
+# cuando se ejecuta la instruccion add $v1, $a0, $a1, luego es 0x00000000 cuando
+# se ejecuta la instruccion jr $ra
+
+# cuestion 2.6
+
+# el contenido de $ra antes y despues de la instruccion jr $ra es cero, encambio
+# pc cambia como se explico anteriormente.
