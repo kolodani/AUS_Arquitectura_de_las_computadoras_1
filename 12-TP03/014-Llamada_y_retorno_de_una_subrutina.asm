c21aedf4d40c6ba4bdc5331d08d58e991a1fa3d6
kolodani <danielvinzia@gmail.com>
2023-06-06 11:06:37 -0300

    assembly, TP03, cuestion 2.5

---
+++ 12-TP03/014-Llamada_y_retorno_de_una_subrutina.asm
@@ -0,0 +1,11 @@
+            .text
+main:
+        li   $a0, 10
+        li   $a1, 20
+        add  $v1, $a0, $a1
+        jr   $ra
+
+# cuestion 2.5
+
+# el contenido de PC y del registro $ra antes y despues de la instruccion jal
+# main es cero
