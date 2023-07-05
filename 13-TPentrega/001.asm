2d8ae0f33600612fd9b78a9bdda8662bd5a0d45f
kolodani <danielvinzia@gmail.com>
2023-07-05 10:37:10 -0300

    assembly, codigo listo

--- 13-TPentrega/001.asm
+++ 13-TPentrega/001.asm
@@ -17,7 +17,7 @@ menu:           .ascii  "\nColecciones de objetos categorizados\n"
                 .ascii  "8-Borrar objeto de la categoria\n"
                 .ascii  "9-Lista de errores\n"
                 .ascii  "0-Salir\n\n"
-                .asciiz "Ingrese la opcion deseada (solo numeros): "
+                .asciiz "Ingrese la opcion deseada (0-9): "
 
 errorMessage:   .asciiz "Error: "
 return:         .asciiz "\n"
@@ -52,7 +52,7 @@ notFound:       .asciiz "\nNo se encontro el objeto\n\n"
         .globl main
 main:
         addi    $sp, $sp, -4
-        li      $t0, 9    #schedv options
+        li      $t0, 9                      #schedv options
         sw      $t0, ($sp)
         jal     initialize_schedv
 
@@ -87,15 +87,15 @@ smalloc:
         jr      $ra
 
 sbrk:
-        li      $a0, 16 # node size fixed 4 words
+        li      $a0, 16                     # node size fixed 4 words
         li      $v0, 9
-        syscall # return node address in v0
+        syscall                             # return node address in v0
         jr      $ra
 
 sfree:
         lw      $t0, slist
         sw      $t0, 12($a0)
-        sw      $a0, slist # $a0 node address in unused list
+        sw      $a0, slist                  # $a0 node address in unused list
         jr      $ra
 
 # a0: list address
@@ -107,17 +107,18 @@ addnode:
         sw      $ra, ($sp)
         sw      $a0, 4($sp)
         jal     smalloc
-        sw      $a1, 4($v0) 	# set node content, if object
-        sw      $a2, 8($v0)  # $a2 = *char to category name
+        sw      $a1, 4($v0)                 # set node content, if object
+        sw      $a2, 8($v0)                 # $a2 = *char to category name
         lw      $a0, 4($sp)
-        lw      $t0, ($a0) 	# first node address
+        lw      $t0, ($a0)                  # first node address
         beqz    $t0, addnode_empty_list
 
 addnode_to_end:
-        lw      $t1, ($t0) 	# last node address
+        lw      $t1, ($t0)                  # last node address
                 # update prev and next pointers of new node
         sw      $t1, 0($v0)
-        sw      $t0, 12($v0)	# update prev and first node to new node
+        sw      $t0, 12($v0)
+                # update prev and first node to new node
         sw      $v0, 12($t1)
         sw      $v0, 0($t0)
         j       addnode_exit
@@ -137,17 +138,17 @@ delnode:
         addi    $sp, $sp, -8
         sw      $ra, ($sp)
         sw      $a0, 4($sp)
-        lw      $a0, 8($a0) 	# get block address
-        jal     sfree 	# free block
-        lw      $a0, 4($sp) 	# restore argument a0
-        lw      $t0, 12($a0) # get next node addres on t0
+        lw      $a0, 8($a0)                 # get block address
+        jal     sfree                       # free block
+        lw      $a0, 4($sp)                 # restore argument a0
+        lw      $t0, 12($a0)                # get next node addres on t0
         beq     $a0, $t0, delnode_point_self
-        lw      $t1, 0($a0) 	# get address to prev node
-        sw      $t1, 0($t0)	# point next node previous, to a0 previous (t1)
-        sw      $t0, 12($t1) # point previous node next, to a0 next
-        lw      $t1, 0($a1) 	# get address of the first node of the list
-        bne     $a0, $t1, delnode_exit # if the list points to the same node that a0
-        sw      $t0, ($a1) 	# point list to next node
+        lw      $t1, 0($a0)                 # get address to prev node
+        sw      $t1, 0($t0)                 # point next node previous, to a0 previous (t1)
+        sw      $t0, 12($t1)                # point previous node next, to a0 next
+        lw      $t1, 0($a1)                 # get address of the first node of the list
+        bne     $a0, $t1, delnode_exit      # if the list points to the same node that a0
+        sw      $t0, ($a1)                  # point list to next node
         j       delnode_exit
 
 delnode_point_self:
@@ -202,18 +203,18 @@ initialize_schedv:
 newcategory:
         addiu   $sp, $sp, -4
         sw      $ra, ($sp)
-        la      $a0, catNameMessage # input category name
+        la      $a0, catNameMessage         # input category name
         jal     getblock
-        move    $a2, $v0 	# $a2 = *char to category name
-        la      $a0, cclist 	# $a0 = list
-        li      $a1, 0 	# $a1 = NULL
+        move    $a2, $v0                    # $a2 = *char to category name
+        la      $a0, cclist                 # $a0 = list
+        li      $a1, 0                      # $a1 = NULL
         jal     addnode
         lw      $t0, wclist
         bnez    $t0, newcategory_end
-        sw      $v0, wclist	 # update working list if was NULL
+        sw      $v0, wclist                 # update working list if was NULL
 
 newcategory_end:
-        li      $v0, 0 	 # return success
+        li      $v0, 0                      # return success
         lw      $ra, ($sp)
         addiu   $sp, $sp, 4
         jr      $ra
@@ -222,7 +223,7 @@ list_categories:
         lw      $t0, cclist
 
 if1:
-        bne     $t0, $0, endif1	#if there are not categories, throw error and return
+        bne     $t0, $0, endif1             #if there are not categories, throw error and return
         la      $a0, errorMessage
         li      $v0, 4
         syscall
@@ -232,7 +233,7 @@ if1:
         jr      $ra
 
 endif1:
-        move    $t1, $t0	#t1 copy of the first node
+        move    $t1, $t0                    #t1 copy of the first node
 
 do1:
         la      $a0, flecha
@@ -244,10 +245,10 @@ do1:
         la      $a0, return
         li      $v0, 4
         syscall
-        lw      $t0, 12($t0)		#t0 = t0 next node
+        lw      $t0, 12($t0)                #t0 = t0 next node
 
 while1:
-        beq     $t0, $t1, endw1	#out of the loop if $t0 == $t1
+        beq     $t0, $t1, endw1             #out of the loop if $t0 == $t1
         j       do1
 
 endw1:
@@ -259,25 +260,25 @@ manage_schedv:
         addi    $sp, $sp, -4
         sw      $ra, ($sp)
         addi    $t0, $a0, -1
-        la      $t1, schedv	#get address of the schedv
-        sll     $t0, $t0, 2	#multiply the choice by 4
-        add     $t0, $t0, $t1 #add choice*4 + schedv_address
-        lw      $t0, ($t0) 	#save contents of the manipulated address onto t0
+        la      $t1, schedv                 #get address of the schedv
+        sll     $t0, $t0, 2                 #multiply the choice by 4
+        add     $t0, $t0, $t1               #add choice*4 + schedv_address
+        lw      $t0, ($t0)                  #save contents of the manipulated address onto t0
         jalr    $t0
         lw      $ra, ($sp)
         addi    $sp, $sp, 4
         jr      $ra
 
 next_category:
-        lw      $t0, wclist 	#get address of the block
+        lw      $t0, wclist                 #get address of the block
         beq     $t0, $0, no_wclist
-        lw      $t1, 12($t0)	#get pointer to next block
+        lw      $t1, 12($t0)                #get pointer to next block
         j       end_select_cat
 
 prev_category:
-        lw      $t0, wclist 	#get address of the block
+        lw      $t0, wclist                 #get address of the block
         beq     $t0, $0, no_wclist
-        lw      $t1, ($t0)	#get pointer to next block
+        lw      $t1, ($t0)                  #get pointer to next block
         j       end_select_cat
 
 end_select_cat:
@@ -307,8 +308,8 @@ no_wclist:
         #a1:content of the object, NULL if it's a category
         #a2:address of the block name
 add_object:
-        lw      $s0, wclist 			#get the address current block pointed to wcl
-        bne     $s0, $0,add_object_if_cat 	#a0 is left intact if there are categories
+        lw      $s0, wclist                 #get the address current block pointed to wcl
+        bne     $s0, $0,add_object_if_cat   #a0 is left intact if there are categories
         la      $a0, errorMessage
         li      $v0, 4
         syscall
@@ -322,22 +323,22 @@ add_object_if_cat:
         sw      $ra, ($sp)
         sw      $s0, 4($sp)
         la      $a0, objNameMessage
-        jal     getblock 	#v0 has the string block
+        jal     getblock                    #v0 has the string block
         lw      $s0, 4($sp)
 
-        la      $a0, 4($s0)	#get the address of the list appended to wclist node
-        move    $a2, $v0 	#put address of the string block onto a2
-        lw      $t0, ($a0)	#get address of the current block pointed by the category pointer
+        la      $a0, 4($s0)                 #get the address of the list appended to wclist node
+        move    $a2, $v0                    #put address of the string block onto a2
+        lw      $t0, ($a0)                  #get address of the current block pointed by the category pointer
 
 if2:
-        bne     $t0, $0, add_last_object # $a0 == NULL,
+        bne     $t0, $0, add_last_object    # $a0 == NULL,
         li      $a1, 1
         jal     addnode
         j       end_add_object
 
 add_last_object:
-        lw      $t0, ($t0)	#take the address of the previous node
-        lw      $a1, 4($t0)	#take the value of the previous node and add 1 to it
+        lw      $t0, ($t0)	                #take the address of the previous node
+        lw      $a1, 4($t0)	                #take the value of the previous node and add 1 to it
         addi    $a1, $a1, 1
         jal     addnode
 
@@ -348,14 +349,14 @@ end_add_object:
 #returns the address of the node added
 
 list_objects:
-        lw      $t0, wclist     #get first category node
+        lw      $t0, wclist                 #get first category node
         beq     $t0, $0, list_obj_no_cat    #if wcl != null && wcl->objlist != null goto do3
         lw      $t1, 4($t0)
         move    $t2, $t1
         beq     $t1, $0, list_obj_no_objs
 
 do3:
-        lw      $a0, 4($t1) 	# save the index
+        lw      $a0, 4($t1)                 # save the index
         li      $v0, 1
         syscall
         la      $a0, flecha
@@ -367,7 +368,7 @@ do3:
         la      $a0, return
         li      $v0, 4
         syscall
-        lw      $t1, 12($t1)	#point to previous node
+        lw      $t1, 12($t1)                #point to previous node
 
 w3:
         beq     $t1, $t2, endw3
@@ -396,7 +397,7 @@ list_obj_no_objs:
 
 del_object:
         lw      $t0, wclist
-        bne     $t0, $0, check_if_objlist #if no categories,jump back
+        bne     $t0, $0, check_if_objlist   #if no categories,jump back
         la      $a0, errorMessage
         li      $v0, 4
         syscall
@@ -404,9 +405,9 @@ del_object:
         jr      $ra
 
 check_if_objlist:
-        addi    $t5, $t0, 4		#get address of the object list
-        lw      $t0, 4($t0)		#$t0 = first node address of the object list
-        bne     $t0, $0, ask_id	#check if there's a node
+        addi    $t5, $t0, 4                 #get address of the object list
+        lw      $t0, 4($t0)                 #$t0 = first node address of the object list
+        bne     $t0, $0, ask_id             #check if there's a node
         la      $a0, errorMessage
         li      $v0, 4
         syscall
@@ -426,12 +427,12 @@ ask_id:
         move    $t7, $v0
 
 search_delete_obj:
-        move    $t1, $t0	#copy the address of the first node to $t1
+        move    $t1, $t0                    #copy the address of the first node to $t1
 
 do4:
-        lw      $t6, 4($t0)	#check if actual node has the index asked
-        beq     $t6, $t7, obj_founded	#if so go to obj_founded
-        lw      $t0, 12($t0)	#else change $t0 to next node address
+        lw      $t6, 4($t0)                 #check if actual node has the index asked
+        beq     $t6, $t7, obj_founded       #if so go to obj_founded
+        lw      $t0, 12($t0)                #else change $t0 to next node address
         beq     $t1, $t0, obj_not_founded
         j       do4
 
@@ -442,8 +443,8 @@ obj_not_founded:
         j       end_del_obj
 
 obj_founded:
-        move    $a0, $t0	#address of the node to remove
-        move    $a1, $t5	#address of the object list
+        move    $a0, $t0                    #address of the node to remove
+        move    $a1, $t5                    #address of the object list
         jal     delnode
 
 end_del_obj:
@@ -454,8 +455,8 @@ end_del_obj:
 del_category:
         #delnode ya se encarga de reapuntar los nodos
         #lo unico que me interesa es pasarle bien la direccion de las listas
-        la      $t0, wclist	#wclist address
-        lw      $t1, ($t0)	#wclist node address
+        la      $t0, wclist                 #wclist address
+        lw      $t1, ($t0)                  #wclist node address
         bne     $t1, $0, del_cat_objects
         la      $a0, errorMessage
         li      $v0, 4
@@ -468,16 +469,16 @@ del_category:
 del_cat_objects:
         addi    $sp, $sp, -12
         sw      $ra, ($sp)
-        sw      $t0, 4($sp)	#save address of wclist
-        sw      $t1, 8($sp) 	#save address of wclist node
-        addi    $t2, $t1, 4	#address of the object list
+        sw      $t0, 4($sp)                 #save address of wclist
+        sw      $t1, 8($sp)                 #save address of wclist node
+        addi    $t2, $t1, 4                 #address of the object list
 
 while5:
-        lw      $t7, ($t2)	#node address of the object list of the cat
-        beq     $t7, $0, del_current_cat	#if the list has no nodes go to delete cat
-        move    $a0, $t7	#set node address
-        move    $a1, $t2	#set list address
-        jal     delnode	#remove node of the list
+        lw      $t7, ($t2)                  #node address of the object list of the cat
+        beq     $t7, $0, del_current_cat    #if the list has no nodes go to delete cat
+        move    $a0, $t7                    #set node address
+        move    $a1, $t2                    #set list address
+        jal     delnode                     #remove node of the list
         j       while5
 
 del_current_cat:
@@ -486,17 +487,17 @@ del_current_cat:
         #If wclist and cclist points to the same block and
         #we remove that block, wclist would be changed with
         #delnode but cclist would point to null
-        lw      $t0, cclist 	#node address of cclist
-        lw      $t0, 12($t0)	#node address of next node
-        sw      $t0, cclist	#set cclist to next node
+        lw      $t0, cclist                 #node address of cclist
+        lw      $t0, 12($t0)                #node address of next node
+        sw      $t0, cclist                 #set cclist to next node
 
-        lw      $a0, 8($sp) 	#set wclist node address
-        lw      $a1, 4($sp) 	#set wclist address
-        jal     delnode	#delete current category from
-                #wclist and set wclist to next node
+        lw      $a0, 8($sp)                 #set wclist node address
+        lw      $a1, 4($sp)                 #set wclist address
+        jal     delnode                     #delete current category from
+        #wclist and set wclist to next node
         lw      $t0, 4($sp)
-        lw      $t1, ($t0) 	#check the content of wclist
-        bne     $t1, $0, end_del_cat	#if wclist is null then there's no categories, thus set cclist to null
+        lw      $t1, ($t0)                  #check the content of wclist
+        bne     $t1, $0, end_del_cat        #if wclist is null then there's no categories, thus set cclist to null
         sw      $0, cclist
         j       end_del_cat
 
