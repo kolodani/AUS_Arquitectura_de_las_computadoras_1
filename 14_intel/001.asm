f90974dbe2d6e724542758eb176974ee84538cc5
kolodani <danielvinzia@gmail.com>
2023-07-05 17:52:07 -0300

    assembly, x86 mi primer programa

---
+++ 14_intel/001.asm
@@ -0,0 +1,20 @@
+; mi primer programa
+;
+
+segment .data
+    integer1        dd      15        ; entero 1
+    integer2        dd      6         ; entero 2
+segment .bss
+    result          resd    1         ; resultado
+segment .text
+    global asm_main
+asm_main:
+                    enter   0,0
+                    pusha
+                    mov     eax, [integer1]
+                    add     eax, [integer2]
+                    mov     [result], eax
+                    popa
+                    mov     eax, 0
+                    leave
+                    ret
