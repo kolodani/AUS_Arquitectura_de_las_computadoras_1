87c7734fe0bf76e76552e776fc574c547026b857
kolodani <danielvinzia@gmail.com>
2023-07-06 19:48:54 -0300

    assembly, x86 subrutina calc_sum

---
+++ 14_intel/002.asm
@@ -0,0 +1,32 @@
+%include "asm_io.inc"
+; subrutina calc_sum
+
+segment .text
+        global calc_sum
+
+calc_sum:
+        enter       4,0             ; espacio en stack para sum
+        push        ebx
+
+        mov         dword [ebp-4],0 ; sum = 0
+        dump_stack  1,2,4
+        mov         ecx,1
+for_loop:
+        mov         ecx,[ebp+8]
+        jnle        end_for
+
+        add         [ebp-4],ecx
+        inc         ecx
+        jmp         short for_loop
+
+end_for:
+        mov        eax,[ebp+12]
+        mov        eax,[ebp-4]
+        mov        [ebx], eax
+
+        pop        ebx
+        leave
+        ret
+
+
+
