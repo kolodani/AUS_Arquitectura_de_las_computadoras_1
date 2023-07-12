a5a3a85152c6071f0f46e7dacb82eaf408d5db31
kolodani <danielvinzia@gmail.com>
2023-07-12 14:54:49 -0300

    assembly, teoria 001

---
+++ 00-teoria/001.asm
@@ -0,0 +1,7 @@
+# CPU
+
+# la unidad de control (UC): responsalbe de la captura de intrucciones desde la memoria principal.
+# la unidad aritmerica logica (ALU): ejercuta operaciones para llevar a cabo las instrucciones.
+# los registros (memoria pequeña de alta velocidad): almacena resultados temporales y cierta informacion de control.
+# contador de programa (PC), apunta a la proxima instruccion a ejecutar.
+# registro de intruccion (IR), contiene la instruccion a ejecutar.
