acb4505b677c2dd3ee05e382e0f213f3217edcde
kolodani <danielvinzia@gmail.com>
2023-07-12 14:58:29 -0300

    assembly, teoria 001.b

--- 00-teoria/001.asm
+++ 00-teoria/001.asm
@@ -5,3 +5,13 @@
 # los registros (memoria pequeña de alta velocidad): almacena resultados temporales y cierta informacion de control.
 # contador de programa (PC), apunta a la proxima instruccion a ejecutar.
 # registro de intruccion (IR), contiene la instruccion a ejecutar.
+
+# Funcionamiento de la CPU
+
+# 1. Pone la proxima instruccion de la memoria en el registro de instruccion (IR).
+# 2. Cambia el contador de programa para que apunte a la instruccion siguiente.
+# 3. Determina el tipo de instruccion.
+# 4. Si la instuccion usa una palabra de memoria, determina donde esta.
+# 5. Copia la palabra, si es necesario, en un registro de la CPU.
+# 6. Ejecuta la instruccion.
+# 7. Vuelve al punto inicial 1 para comenzar con la siguiente instruccion.
