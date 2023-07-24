659355c19b52a686cccfd6ce145c07238c3642e7
Daniel Alberto Vinzia <danielvinzia@gmail.com>
2023-07-24 14:55:19 -0300

    assembly, teoria 005

---
+++ 00-teoria/005.asm
@@ -0,0 +1,13 @@
+# Unidad Aritmetica Logica
+
+# Unidad Aritmetica Logica
+# Bloque combinacional
+# ALU 32 bits formada por 32 ALUs de 1 bit
+# Operaciones aritmeticas, logicas y comparaciones
+# Suma (add y Variantes) ALU = A + B
+# Resta (sub y Variantes) ALU = A + (-B) = A + (B' + 1)
+# AND (and y sus variantes) ALU = A AND B
+# OR (or y sus variantes) ALU = A OR B
+# Deteccion de igualdad (beq) si A = B -> ZERO = 1
+# Deteccion de menor (slt): si A - B < 0 -> ALU = 00...01
+# Deteccion de desbordamiento
